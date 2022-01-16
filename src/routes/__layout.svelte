7e37f3369b00f01d6254b9edae436d1ad40038ff
George Ungureanu Vranceanu <gungureanuvranceanu@gmail.com>
2022-01-17 00:05:01 +0200

    Finish index and about

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import 'papercss/dist/paper.min.css';
+	import cv from '/static/CV.pdf';
+</script>
+
+<nav class="border fixed split-nav">
+	<div class="nav-brand">
+		<h3><a href="/">© George U.V.</a></h3>
+	</div>
+	<div class="collapsible">
+		<input id="collapsible1" type="checkbox" name="collapsible1" />
+		<label for="collapsible1">Menu</label>
+		<div class="collapsible-body">
+			<ul class="inline">
+				<li><a href="/about">About</a></li>
+				<li><a href="/portfolio">Portfolio</a></li>
+				<li><a href={cv} rel="external">Résumé</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<slot />
+
+<style>
+	div > label {
+		font-size: 1em;
+	}
+	li > a {
+		font-size: 1.5em;
+	}
+</style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,260 @@
+<script>
+	import photo from '/static/photo.jpg';
+</script>
+
+<main id>
+	<h1 id="aboutMe">About Me</h1>
+	<section id="picture">
+		<img src={photo} alt="George in Central London" />
+	</section>
+
+	<section class="card" id="intro">
+		<h2>Introduction</h2>
+		<article>
+			<p>
+				My name is <span style="font-weight: bold;">George Ungureanu Vrânceanu</span>. If you can
+				not pronounce that, do not worry, I can barely pronounce it myself.
+			</p>
+			<p>
+				I am a 21-year-old Romanian (soon to be) Software Engineer, who immigrated to the United
+				Kingdom in order to study computing at Imperial College London. I have always loved
+				computers and I have been passionate about technology from a very young age.
+			</p>
+			<p>
+				My hobbies include handball refereeing, going to the gym, playing video games (my dream is
+				to become a game developer one day) and, at the risk of sounding a bit cliché, programming.
+			</p>
+			<p>
+				I always try to be active both on GitHub, where I am always working on a project (this
+				little website included), and on social media, so feel free to give me a star or a follow at
+				the links below:
+			</p>
+			<div id="links">
+				<a
+					href="https://www.linkedin.com/in/george-ungureanu-vranceanu-b11957185/"
+					target="_blank"
+					rel="noreferrer"><img src="/static/in.svg" alt="linkedin" /></a
+				>
+				<a href="https://github.com/Gungy2" target="_blank" rel="noreferrer"
+					><img src="/static/git.svg" alt="github" /></a
+				>
+				<a
+					href="https://www.facebook.com/george.ungureanuvranceanu"
+					target="_blank"
+					rel="noreferrer"><img src="/static/fb.svg" alt="facebook" /></a
+				>
+				<a href="https://www.instagram.com/george_1011" target="_blank" rel="noreferrer"
+					><img src="/static/insta.svg" alt="instagram" /></a
+				>
+			</div>
+		</article>
+	</section>
+	<section class="card" id="education">
+		<h2 class="card-title">Education</h2>
+		<article>
+			<h3 class="card-subtitle">
+				<a href="https://www.imperial.ac.uk/" target="_blank" rel="noreferrer">
+					Imperial College London
+				</a>
+			</h3>
+
+			<p>
+				Currently I am a third year student at Imperial College London, working towards a MEng in
+				Computing. I finished my first year with as
+				<span style="font-style: italic;">first class</span>
+				(84 overall), while also being on the
+				<span style="font-weight: bold; font-style: italic;">Dean's List</span>. Here I have gained
+				strong knowledge in a wide variety of subjects including data structures, networking and
+				theoretical computer science. I expect to graduate in 2023.
+			</p>
+
+			<h3 class="card-subtitle">
+				<a href="https://www.cnvranceanu.ro/" target="_blank" rel="noreferrer">
+					National College "Gh. Vranceanu" Bacau, Romania
+				</a>
+			</h3>
+
+			<p>
+				From 2015 to 2019, I studied at the best highschool in my home city, where I established my
+				foundations in Mathematics and Science.
+			</p>
+		</article>
+	</section>
+	<section class="card" id="work">
+		<h2>Work Experience</h2>
+		<article>
+			<p>
+				I have no work experience yet, so I am open to any opportunities, especially summer
+				internships.
+			</p>
+			<p>
+				If you want to see some of my most notable projects, go to my <a href="/portfolio"
+					>portfolio</a
+				> page.
+			</p>
+			<p>
+				On the other hand, if I already seem like a good developer to you, go ahead and send me a
+				message in the <a href="/contact">contact</a> page or at my
+				<a href="mailto:gungureanuvranceanu@gmail.com">email address</a>.
+			</p>
+		</article>
+	</section>
+	<section class="card" id="tech">
+		<h2>Known Technologies</h2>
+		<article>
+			<p>
+				Here are some programming languages and technologies that I have learnt and practiced over
+				the years. Even though I have not used them in a professional environment yet, I am
+				confident that I would be up to speed with any team in a very short time.
+			</p>
+			<ul>
+				<li><img src="/static/java.svg" alt="Java" />Java</li>
+				<li><img src="/static/c.svg" alt="C" />C</li>
+				<li><img src="/static/python.svg" alt="Python" />Python</li>
+				<li><img src="/static/haskell.svg" alt="Haskell" />Haskell</li>
+				<li><img src="/static/js.svg" alt="JavaScript" />JavaScript</li>
+				<li><img src="/static/react.svg" alt="React" />React</li>
+				<li><img src="/static/html.svg" alt="HTML5" />HTML5</li>
+				<li><img src="/static/css.svg" alt="CSS" />CSS</li>
+				<li><img src="/static/vc.svg" alt="Git" />Git</li>
+				<li><img src="/static/sql.svg" alt="SQL" />SQL</li>
+				<li><img src="/static/elm.svg" alt="Elm" />Elm</li>
+				<li><img src="/static/rust.png" alt="Rust" />Rust</li>
+				<li><img src="/static/solidity.svg" alt="Solidity" />Solidity</li>
+				<li><img src="/static/kotlin.png" alt="Kotlin" />Kotlin</li>
+			</ul>
+		</article>
+	</section>
+</main>
+
+<style>
+	.card {
+		padding: 20px;
+		width: 90vw;
+		background-color: rgb(245, 245, 245, 0.4);
+		margin: auto;
+		margin-bottom: 30px;
+	}
+
+	.card:hover {
+		background-color: rgb(245, 245, 245, 1);
+	}
+
+	#tech img {
+		margin: auto;
+		margin-bottom: 10px;
+		height: 30px;
+		width: 30px;
+	}
+
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		padding-left: 0;
+		padding-right: 0;
+	}
+
+	ul li::before {
+		display: none;
+	}
+
+	ul li {
+		text-align: center;
+		margin: 10px 30px;
+	}
+
+	h1 {
+		margin-top: 90px;
+		text-align: center;
+		font-size: 6em;
+	}
+
+	h2 {
+		margin: 20px 0;
+	}
+
+	#picture {
+		margin-bottom: 30px;
+		height: auto;
+		margin: 0 30px;
+		margin-bottom: 30px;
+	}
+
+	a {
+		color: #41403e;
+	}
+
+	#links {
+		display: flex;
+		justify-content: space-around;
+		flex-wrap: wrap;
+	}
+
+	#links > a {
+		background-image: none;
+	}
+
+	#links > a > img {
+		width: 50px;
+	}
+
+	@media only screen and (min-width: 768px) {
+		main {
+			margin: auto;
+			display: grid;
+			width: 90vw;
+			gap: 40px;
+			grid-template-columns: 1fr 1fr;
+			grid-template-areas:
+				'about about'
+				'intro   pic'
+				'edu     pic'
+				'edu   work'
+				'tech  tech ';
+		}
+
+		p {
+			font-size: 1.2em;
+		}
+
+		#tech img {
+			margin: auto;
+			margin-bottom: 10px;
+			height: 50px;
+			width: 50px;
+		}
+
+		ul li {
+			margin: 20px 50px;
+		}
+
+		.card {
+			width: auto;
+		}
+
+		#aboutMe {
+			grid-area: about;
+		}
+
+		#work {
+			grid-area: work;
+		}
+
+		#tech {
+			grid-area: tech;
+		}
+
+		#education {
+			grid-area: edu;
+		}
+
+		#picture {
+			grid-area: pic;
+		}
+
+		#intro {
+			grid-area: intro;
+		}
+	}
+</style>
