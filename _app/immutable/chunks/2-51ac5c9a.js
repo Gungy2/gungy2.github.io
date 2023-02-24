3101f9e96bf4afea9fbf4010a2162a0f25ae8bee
Gungy2 <Gungy2@users.noreply.github.com>
2023-02-24 10:53:25 +0000

    deploy: 9f3bb0cd9a5de4030cbca61c91f773d8f8fbdd4b

---
+++ _app/immutable/chunks/2-51ac5c9a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-e6fef483.js";const e=!0;export{t as component,e as server};
---
+++ _app/immutable/components/pages/demo/_page.svelte-02b44870.js
@@ -0,0 +1,1 @@
+import{S as e,i as t,s as a}from"../../../chunks/index-abf3d057.js";class o extends e{constructor(s){super(),t(this,s,null,null,a,{})}}export{o as default};
