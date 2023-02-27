077507583de3763237e6aa49934717738a4b4b61
George Ungureanu Vranceanu <gungureanuvranceanu@gmail.com>
2023-02-27 14:24:33 +0100

    Change link

--- src/routes/demo/+layout.server.ts
+++ src/routes/demo/+layout.server.ts
@@ -2,5 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
  
 export const load = (() => {
-  throw redirect(301, 'https://www.google.com');
+  throw redirect(301, 'https://youtu.be/vHNp8lg-79U');
 }) satisfies LayoutServerLoad;
